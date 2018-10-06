f617a5b6cbce0ba7d8e227579bdb2bf3f359d5d1
tianjinran <43253477+tianjinran@users.noreply.github.com>
2018-10-06 00:25:20 -0700

    Create Student.java

---
+++ Assignment3/Problem1/Student.java
@@ -0,0 +1,22 @@
+class Student {
+
+    private String name;
+    private int id;
+
+    //constructor.
+    public Student(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    //Getter for name. return the current "name" data.
+    public String getName(){
+        return name;
+    }
+
+    //Getter for id. return the current "id " data.
+    public int getId(){
+        return id;
+    }
+
+}
